60023f9ef7fc4a1028f15fd45303e4dd6850e221
Silencio <77356433+S1lencio@users.noreply.github.com>
2025-04-12 11:16:15 +0200

    initial commit

---
+++ src/main/java/de/silencio/better_beacon_range/BetterBeaconRange.java
@@ -0,0 +1,10 @@
+package de.silencio.better_beacon_range;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterBeaconRange implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/de/silencio/better_beacon_range/mixin/BeaconRangeMixin.java
@@ -0,0 +1,62 @@
+package de.silencio.better_beacon_range.mixin;
+
+import net.minecraft.block.entity.BeaconBlockEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Objects;
+
+@Mixin(BeaconBlockEntity.class)
+public class BeaconRangeMixin {
+
+    @Inject(method = "applyPlayerEffects", at = @At("HEAD"), cancellable = true)
+    private static void modifyBeaconRange(World world, BlockPos pos, int beaconLevel, @Nullable RegistryEntry<StatusEffect> primaryEffect, @Nullable RegistryEntry<StatusEffect> secondaryEffect, CallbackInfo ci) {
+        double base = 50;
+        double perLevel = 25;
+        double customRange = beaconLevel * perLevel + base;
+
+        ci.cancel();
+
+        applyPlayerEffects(world, pos, beaconLevel, primaryEffect, secondaryEffect, customRange);
+    }
+
+    @Unique
+    private static void applyPlayerEffects(World world, BlockPos pos, int beaconLevel,
+                                           @Nullable RegistryEntry<StatusEffect> primaryEffect,
+                                           @Nullable RegistryEntry<StatusEffect> secondaryEffect,
+                                           double customRange) {
+
+        if (!world.isClient && primaryEffect != null) {
+            int i = 0;
+            if (beaconLevel >= 4 && Objects.equals(primaryEffect, secondaryEffect)) {
+                i = 1;
+            }
+
+            int j = (9 + beaconLevel * 2) * 20;
+            Box box = new Box(pos).expand(customRange).stretch(0.0, world.getHeight(), 0.0);
+            List<PlayerEntity> list = world.getNonSpectatingEntities(PlayerEntity.class, box);
+
+            for (PlayerEntity playerEntity : list) {
+                playerEntity.addStatusEffect(new StatusEffectInstance(primaryEffect, j, i, true, true));
+            }
+
+            if (beaconLevel >= 4 && !Objects.equals(primaryEffect, secondaryEffect) && secondaryEffect != null) {
+                for (PlayerEntity playerEntity : list) {
+                    playerEntity.addStatusEffect(new StatusEffectInstance(secondaryEffect, j, 0, true, true));
+                }
+            }
+        }
+    }
+}
